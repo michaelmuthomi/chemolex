08c2ed33b8229237b6ffe9e3b3a02b4625e78fa4
michaelmuthomi <lit911call@gmail.com>
2025-01-12 15:49:49 +0300

    refactor: improve content and styling

--- src/routes/aboutus.tsx
+++ src/routes/aboutus.tsx
@@ -28,20 +28,28 @@ function AboutUs() {
     <div className="flex flex-col w-full">
       <section className="flex gap-2 items-center sticky px-4 top-0 bg-background py-4 z-10 border-b-[1px]">
         <SidebarTrigger />
-        <Header location="Employees" />
+        <Header location="About Us" />
       </section>
       <section className="px-6 pt-4 grid gap-10">
         <div className="grid gap-2">
-          <h1 className="text-3xl font-bold text-neutral-300">About Us</h1>
-          <p className="font-light text-zinc-400 w-1/2">
-            REFNET AIR CONDITIONING (E.A) LTD has been involved in air
-            conditioning works in Kenya and its neighboring countries like
-            Tanzania, Uganda & Rwanda. We provide quality and timely services,
-            making us a trusted name in the industry.
-          </p>
+          <h1 className="text-4xl font-bold w-2/3 pt-10 text-neutral-300">
+            Elevating Comfort: Your Trusted Partner in Air Conditioning
+            Solutions Across East Africa
+          </h1>
+          <div className="flex gap-4 px-4 py-6">
+            <div className="w-14 h-[2px] bg-neutral-600 mt-2"> </div>
+            <p className="font-light leading-loose text-xl text-neutral-500 w-3/4">
+              At RefNet Air Conditioning, we pride ourselves on being a leading
+              provider of innovative refrigeration and air conditioning
+              solutions in East Africa. With years of industry experience, our
+              dedicated team of professionals is committed to delivering
+              high-quality services tailored to meet the unique needs of our
+              clients.
+            </p>
+          </div>
         </div>
         <div className="grid gap-2">
-          <h2 className="text-2xl font-bold text-neutral-300">Our Mission</h2>
+          <h2 className="text-2xl font-bold text-neutral-300">Trusted by</h2>
           <p className="font-light text-zinc-400">
             To work closely with clients while emphasizing time and quality of
             service delivery.
