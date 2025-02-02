27a71f2b7e2f0e66cd7f3eb83f1e71deb661710e
michaelmuthomi <lit911call@gmail.com>
2025-02-02 18:27:23 +0300

    refactor: update terminology from Customers to Mentees

--- src/routes/customers.tsx
+++ src/routes/customers.tsx
@@ -30,7 +30,7 @@ function ManageUsers() {
       setCardData([
         {
           icon: <Users size={20} color="black" />,
-          title: "Total Customers",
+          title: "Total Mentees",
           statistic: data.length,
           moreDetails: "The total amount of all orders.",
         },
@@ -60,13 +60,13 @@ function ManageUsers() {
     <div className="flex flex-col w-full">
       <section className="flex gap-2 items-center sticky px-4 top-0 bg-background py-4 z-10 border-b-[1px]">
         <SidebarTrigger />
-        <Header location="Customers" />
+        <Header location="Mentees" />
       </section>
       <section className="px-6 pt-8 grid gap-10">
         <div>
-          <h1 className="text-3xl font-bold text-neutral-300">Customers</h1>
+          <h1 className="text-3xl font-bold text-neutral-300">Mentees</h1>
           <p className="font-medium text-zinc-600">
-            A list of all the customers in the database, view and manage their
+            A list of all the Mentees in the database, view and manage their
             status.
           </p>
         </div>
